61a5961bbe63a2a1ab855b495f4df73d16bc5de8
Marko-What <markoFindJob@gmail.com>
2017-07-02 22:29:34 +0200

    Create pulse.js

---
+++ pulse.js
@@ -0,0 +1,18 @@
+(function(){
+
+		$('div#wrapper').on('click', function(){
+			$('div#main span.first').animate({'width': '52px'}, 60, 'linear', function(){
+		
+	$('div#main1 span.second').animate({'width': '148px'}, 100, 'linear', function(){
+			$('div#main2 span.third').animate({'width': '190px'}, 110, 'linear', function(){
+					$('div#main3 span.fourth').animate({'width': '280px'}, 200, 'linear', function(){
+						$('div#main4 span.fifth').animate({'width':'210px'}, 200, 'linear', function(){
+							$('div#main5 span.sixth').animate({'width':'80px'}, 200, 'linear');
+					});
+				});
+			});
+		});
+	});
+});
+
+})();
